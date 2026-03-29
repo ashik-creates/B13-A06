007b85889f9a2adb63feb22acb254b6c3a50e6f0
ashikur rahman <ashikorrahman8900@gmail.com>
2026-03-30 01:30:45 +0600

    some fixing in banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,13 +5,13 @@ import bannerImg from "../../assets/banner.png";
 const Banner = () => {
   return (
     <div className="max-w-[95%] md:w-11/12 mx-auto min-h-162.5 flex items-center justify-center mt-5">
-      <div className="flex flex-col md:grid md:grid-cols-12 items-center space-y-20 gap-0 md:gap-10">
-        <div className="text-center md:text-start col-span-7 items-center space-y-5">
+      <div className="flex flex-col md:grid md:grid-cols-12 items-center gap-10">
+        <div className="text-center md:text-start col-span-1 items-center md:items-start md:col-span-7 justify-center flex flex-col gap-5">
           <div className="badge bg-purple-200 text-[#4F39F6] rounded-full max-[300px]:hidden">
             <FaRegDotCircle></FaRegDotCircle> New: AI-Powered Tools Available
           </div>
           <h1 className="text-4xl md:text-5xl lg:text-7xl font-extrabold">Supercharge Your Digital Workflow</h1>
-          <p className="max-w-122 text-gray-500">
+          <p className="md:max-w-122 text-gray-500">
             Access premium AI tools, design assets, templates, and productivity
             software—all in one place. Start creating faster today. Explore
             Products
@@ -23,7 +23,7 @@ const Banner = () => {
             </button>
           </div>
         </div>
-        <div className="col-span-5 w-full max-[768px]:h-100">
+        <div className="col-span-1 md:col-span-5 w-full max-[768px]:h-100">
           <img src={bannerImg} alt="banner image" className="w-full overflow-hidden"/>
         </div>
       </div>
